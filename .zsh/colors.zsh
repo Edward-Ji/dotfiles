cec1e73c6c11bdb585e2f41f992df2f471a14f20
Edward Ji <jiziao6@gmail.com>
2021-09-09 11:11:47 +1000

    Auto backup

--- .zsh/colors.zsh
+++
@@ -1,5 +0,0 @@
-# ls colors
-export LSCOLORS='Gxfxbxdxcxegedhbhghchd'
-
-# grep highlight color
-export GREPCOLOR='auto'
--- backup.sh
+++ backup.sh
@@ -8,19 +8,16 @@ files=".hushlogin .vimrc .vim/after/ftplugin .zsh .zshenv .zshrc"
 
 # Config directory (defauls to home directory)
 config_dir="${XDG_CONFIG_DIR:-$HOME}"
-echo "Config directory: $config_dir"
 
 # Copy and add all file
 for file in $files
 do
     if [[ -d "$config_dir/$file" ]]
     then
-        echo "Copying directory $config_dir/$file and its contents"
-        mkdir -p "./$file" && cp -R "$config_dir/$file/" "./$file"
+        mkdir -p "./$file" && ln "$config_dir/$file/*" "./$file"
     elif [[ -f "$config_dir/$file" ]]
     then
-        echo "Copying file $config_dir/$file"
-        mkdir -p `dirname "$file"` && cp "$config_dir/$file" "./$file"
+        mkdir -p `dirname "$file"` && ln "$config_dir/$file" "./$file"
     else
         echo "Missing file $config_dir/$file"
         break
@@ -28,10 +25,7 @@ do
     git add "$file"
 done
 
-# Add this script
-echo "Adding backup script"
-git add backup.sh
-
 # Commit and push
+git add -A
 git commit -m "${1:-Auto backup}"
 git push -u origin main
