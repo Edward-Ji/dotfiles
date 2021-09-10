6b1ad9243db3744eaafb28f1f4df9f02e27deabc
Edward Ji <jiziao6@gmail.com>
2021-09-10 21:12:35 +1000

    Update bd command to go up one level without argument

--- .zsh/bd.zsh
+++ .zsh/bd.zsh
@@ -1,66 +1,69 @@
-# wget https://raw.githubusercontent.com/Tarrasch/zsh-bd/master/bd.zsh
+# Source https://raw.githubusercontent.com/Tarrasch/zsh-bd/master/bd.zsh
+# Altered for my own use.
 
-bd () {
-  (($#<1)) && {
-    print -- "usage: $0 <name-of-any-parent-directory>"
-    print -- "       $0 <number-of-folders>"
-    return 1
-  } >&2
-  # example:
-  #   $PWD == /home/arash/abc ==> $num_folders_we_are_in == 3
-  local num_folders_we_are_in=${#${(ps:/:)${PWD}}}
-  local dest="./"
-
-  # First try to find a folder with matching name (could potentially be a number)
-  # Get parents (in reverse order)
-  local parents
-  local i
-  for i in {$num_folders_we_are_in..2}
-  do
-    parents=($parents "$(echo $PWD | cut -d'/' -f$i)")
-  done
-  parents=($parents "/")
-  # Build dest and 'cd' to it
-  local parent
-  foreach parent (${parents})
-  do
-    dest+="../"
-    if [[ $1 == $parent ]]
+function bd {
+    # If no argument is provided go up one level
+    if [[ -z "$1" ]]
     then
-      cd $dest
-      return 0
+        cd ..
+        return 0
     fi
-  done
 
-  # If the user provided an integer, go up as many times as asked
-  dest="./"
-  if [[ "$1" = <-> ]]
-  then
-    if [[ $1 -gt $num_folders_we_are_in ]]
-    then
-      print -- "bd: Error: Can not go up $1 times (not enough parent directories)"
-      return 1
-    fi
-    for i in {1..$1}
+    # Prepare all parent directory names and count max level
+    local level_max=${#${(ps:/:)${PWD}}}
+    local i
+    local parents
+    for i in {$level_max..2}
     do
-      dest+="../"
+        parents=($parents "$(echo $PWD | cut -d / -f $i)")
     done
-    cd $dest
-    return 0
-  fi
+    parents=($parents "/")
 
-  # If the above methods fail
-  print -- "bd: Error: No parent directory named '$1'"
-  return 1
+    local dest="./"
+
+    # If argument provided is an integer, got up that many levels
+    dest="./"
+    if [[ "$1" = <-> ]]
+    then
+        if [[ $1 -gt $level_max ]]
+        then
+            print -- "$0: Error: Can not go up $1 times"
+            return 1
+        fi
+        for i in {1..$1}
+        do
+            dest+="../"
+        done
+        cd $dest
+        return 0
+    fi
+
+    # If argument is a parent directory name, do up to the directory
+    local parent
+    foreach parent (${parents})
+    do
+        dest+="../"
+        if [[ $1 == $parent ]]
+        then
+            cd $dest
+            return 0
+        fi
+    done
+
+    # All above methods fail signals invalid argument
+    print -- "$0: Error: No parent directory named '$1'"
+    return 2
 }
-_bd () {
-  # Get parents (in reverse order)
-  local num_folders_we_are_in=${#${(ps:/:)${PWD}}}
-  local i
-  for i in {$num_folders_we_are_in..2}
-  do
-    reply=($reply "`echo $PWD | cut -d'/' -f$i`")
-  done
-  reply=($reply "/")
+
+# Get autocomplete options
+function _bd_comp {
+    # Get parents (in reverse order)
+    local level_max=${#${(ps:/:)${PWD}}}
+    local i
+    for i in {$level_max..2}
+    do
+        reply=($reply "`echo $PWD | cut -d'/' -f$i`")
+    done
+    reply=($reply "/")
 }
-compctl -V directories -K _bd bd
+compctl -V directories -K _bd_comp bd
