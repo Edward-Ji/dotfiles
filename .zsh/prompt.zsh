c143094264fb7ea430bd6cf136f0da44c37dfca1
Edward Ji <jiziao6@gmail.com>
2021-09-28 02:38:34 +1000

    Simplify prompt vcs to branch and action
    Previously git action format is default to something like
     (git)-[master|merge]-
    which is very unelegant.

--- .zsh/prompt.zsh
+++ .zsh/prompt.zsh
@@ -1,5 +1,6 @@
 # prompt end of line mark
 PROMPT_EOL_MARK='%F{7}\%f'
+
 # === prompt ===
 setopt PROMPT_SUBST
 
@@ -7,11 +8,12 @@ setopt PROMPT_SUBST
 PROMPT='%B%3~%b '
 
 # git integration
+zstyle ':vcs_info:git*' formats '%b'
+zstyle ':vcs_info:git*' actionformats '%b (%a)'
 autoload -Uz vcs_info
 precmd_vcs_info() { vcs_info }
 precmd_functions+=( precmd_vcs_info )
 PROMPT+='${vcs_info_msg_0_:+"⑂ $vcs_info_msg_0_ "}'
-zstyle ':vcs_info:git:*' formats '%b'
 
 # exit code and superuser integration
 PROMPT+='%(?.%F{green}.%F{red})❯%(!.❯.)%f '
