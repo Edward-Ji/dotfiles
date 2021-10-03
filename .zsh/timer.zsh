820a63f2611c55833957dc348a40641fa9985ee1
Edward Ji <jiziao6@gmail.com>
2021-10-04 01:05:27 +1100

    Add zsh timer minimum threshold
    Add documentation to zsh timer script

--- .zsh/timer.zsh
+++ .zsh/timer.zsh
@@ -1,3 +1,14 @@
+################################################################################
+# ZSH Execution Timer                                                          #
+# This script utilizes the zsh pre-execute and pre-command functions to keep   #
+# track of the execution time of interactive commands. The result will be      #
+# printed out in seconds if it is greater than a set threshold.                #
+#                                                                              #
+# Environment variables                                                        #
+# TIMER_MINIMUM -- only prints the elapsed time if it is greater than this     #
+#   threshold, defaults to 3                                                   #
+################################################################################
+
 preexec_timer() {
     timer=${timer:-$SECONDS}
 }
@@ -7,9 +18,11 @@ precmd_timer() {
     then
         local elapsed=$(( $SECONDS - $timer ))
         unset timer
-        [[ $elapsed -ne 0 ]] && printf "⌚︎%ds\n" $elapsed
+        [[ $elapsed -gt $TIMER_MINIMUM ]] && printf "⌚︎%ds\n" $elapsed
     fi
 }
+
+[[ -z $TIMER_MINIMUM ]] && export TIMER_MINIMUM=3
 
 preexec_functions+=(preexec_timer)
 precmd_functions+=(precmd_timer)
