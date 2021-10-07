Edward-Ji/dotfiles

commit f3ecaac80b48d2c89b4b8cdbf27f1d4c0e31769d

Make bash right prompt part of bash prompt
I previously used `printf` to display right prompt before prompt. Now,
the right prompt is integrated into the standard bsah prompt `PS1`.

.bash_profile
@@ -33,6 +33,6 @@ precmd() {
 
     rprompt="${USER}@${HOSTNAME:-$(hostname)}"
     local offsetcursor="\e[$((${COLUMNS} - ${#rprompt}))G"
-    printf "${savecursor}${offsetcursor}${gray}${rprompt}${reset}${restorecursor}"
+    PS1="\[${savecursor}${offsetcursor}${gray}${rprompt}${reset}${restorecursor}${PS1}\]"
 }
 

commit bdeb81244f5c0ff675cc5a1b4e420679a9a16543

Fix bash input overwrite issue
Resolved the bug caused by commit f3ecaac80b48d2c89b4b8cdbf27f1d4c0e31769d.
Integrating right prompt with PS1 cause the second line of input
overwriting the first line instead of starting on a new line.

.bash_profile
@@ -33,6 +33,6 @@ precmd() {
 
     rprompt="${USER:-$(whoami)}@${HOSTNAME:-$(hostname)}"
     local offsetcursor="\e[$((${COLUMNS} - ${#rprompt}))G"
-    PS1="\[${savecursor}${offsetcursor}${gray}${rprompt}${reset}${restorecursor}${PS1}\]"
+    printf "${savecursor}${offsetcursor}${gray}${rprompt}${reset}${restorecursor}"
 }
 