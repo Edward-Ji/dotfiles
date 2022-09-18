30b3c22e839be023555ff1d9f0e160d7af17989b
Edward Ji <jiziao6@gmail.com>
2022-09-18 15:19:55 +1000

    Add vim editing mode in iPython config

--- .ipython/profile_default/ipython_config.py
+++ .ipython/profile_default/ipython_config.py
@@ -1,3 +1,8 @@
 ## Automatically set the terminal title
 #  Default: True
 c.TerminalInteractiveShell.term_title = False
+
+## Use vi for editing
+c.TerminalInteractiveShell.editing_mode = 'vi'
+c.TerminalInteractiveShell.emacs_bindings_in_vi_insert_mode = False
+c.TerminalInteractiveShell.timeoutlen = 0.25
