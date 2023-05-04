91f812e8279e1c4edf282b0a88e1bc05900c58c3
Edward Ji <jiziao6@gmail.com>
2023-05-04 16:40:01 +1000

    Fix neovim configuration whitespaces

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -3,21 +3,21 @@ local builtin = require('telescope.builtin')
 local actions = require('telescope.actions')
 
 telescope.setup{
-	defaults = {
-		mappings = { i = { ['<esc>'] = actions.close } }
-	},
-	pickers = {
-		find_files = {
-			find_command = { 
-				'rg', '--files', '--hidden', '--glob', '!**/.git/*'
-			}
-		},
+    defaults = {
+        mappings = { i = { ['<esc>'] = actions.close } }
+    },
+    pickers = {
+        find_files = {
+            find_command = {
+                'rg', '--files', '--hidden', '--glob', '!**/.git/*'
+            }
+        },
         live_grep = {
             additional_args = function(opts)
                 return {"--hidden"}
             end
         },
-	}
+    }
 }
 
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
--- .config/nvim/lua/eji/packer.lua
+++ .config/nvim/lua/eji/packer.lua
@@ -2,8 +2,8 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-	-- Packer can manage itself
-	use 'wbthomason/packer.nvim'
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
 
     use { "catppuccin/nvim", as = "catppuccin" }
 
@@ -32,34 +32,34 @@ return require('packer').startup(function(use)
     }
 
     use {
-		'nvim-treesitter/nvim-treesitter',
-		run = ':TSUpdate'
-	}
+        'nvim-treesitter/nvim-treesitter',
+        run = ':TSUpdate'
+    }
 
-	use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
-	use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.1',
-		requires = { {'nvim-lua/plenary.nvim'} }
-	}
-	
-	use {
-		'VonHeikemen/lsp-zero.nvim',
-		branch = 'v2.x',
-		requires = {
-			-- LSP Support
-			{'neovim/nvim-lspconfig'},             -- Required
-			{                                      -- Optional
-			'williamboman/mason.nvim',
-			run = function()
-				pcall(vim.cmd, 'MasonUpdate')
-			end,
-		},
-		{'williamboman/mason-lspconfig.nvim'}, -- Optional
+    use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.1',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
+    
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v2.x',
+        requires = {
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},             -- Required
+            {                                      -- Optional
+            'williamboman/mason.nvim',
+            run = function()
+                pcall(vim.cmd, 'MasonUpdate')
+            end,
+        },
+        {'williamboman/mason-lspconfig.nvim'}, -- Optional
 
-		-- Autocompletion
-		{'hrsh7th/nvim-cmp'},     -- Required
-		{'hrsh7th/cmp-nvim-lsp'}, -- Required
-		{'L3MON4D3/LuaSnip'},     -- Required
-	}
+        -- Autocompletion
+        {'hrsh7th/nvim-cmp'},     -- Required
+        {'hrsh7th/cmp-nvim-lsp'}, -- Required
+        {'L3MON4D3/LuaSnip'},     -- Required
+    }
 }
 end)
