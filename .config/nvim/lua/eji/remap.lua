b4647213f50a63b7aa2e74d3f0ad1bca12c4f39d
Edward Ji <jiziao6@gmail.com>
2023-05-04 16:36:21 +1000

    Change move line or selection neovim remap

--- .config/nvim/lua/eji/remap.lua
+++ .config/nvim/lua/eji/remap.lua
@@ -1,4 +1,7 @@
 vim.g.leadermap = '\\'
 
-vim.keymap.set('v', 'J', ":move '>+1<CR>gv=gv")
-vim.keymap.set('v', 'K', ":move '<-2<CR>gv=gv")
+vim.keymap.set('n', '<M-j>', "V:move '>+1<CR>gv=")
+vim.keymap.set('n', '<M-k>', "V:move '<-2<CR>gv=")
+
+vim.keymap.set('v', '<M-j>', ":move '>+1<CR>gv=gv")
+vim.keymap.set('v', '<M-k>', ":move '<-2<CR>gv=gv")
