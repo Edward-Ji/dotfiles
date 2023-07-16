3db8e0a052cf2fa5e46eb141368b730fe78a8619
Edward Ji <jiziao6@gmail.com>
2023-07-16 11:58:03 +0800

    Improve move lines up or down maps

--- .config/nvim/lua/eji/remap.lua
+++ .config/nvim/lua/eji/remap.lua
@@ -1,5 +1,5 @@
-vim.keymap.set('n', '<M-j>', "V:move '>+1<CR>gv=")
-vim.keymap.set('n', '<M-k>', "V:move '<-2<CR>gv=")
+vim.keymap.set('n', '<M-j>', ":move .+1<CR>==", {silent = true})
+vim.keymap.set('n', '<M-k>', ":move .-2<CR>==", {silent = true})
 
-vim.keymap.set('v', '<M-j>', ":move '>+1<CR>gv=gv")
-vim.keymap.set('v', '<M-k>', ":move '<-2<CR>gv=gv")
+vim.keymap.set('v', '<M-j>', ":move '>+1<CR>gv=gv", {silent = true})
+vim.keymap.set('v', '<M-k>', ":move '<-2<CR>gv=gv", {silent = true})
