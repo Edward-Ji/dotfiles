bee10b96fcb9a89a0452566a42bb4fec0d1a01c4
Edward Ji <jiziao6@gmail.com>
2023-07-16 11:56:55 +0800

    Customize lualine look some more

--- .config/nvim/after/plugin/lualine.lua
+++ .config/nvim/after/plugin/lualine.lua
@@ -1,10 +1,27 @@
 require('lualine').setup {
     options = {
         theme = 'auto',
-        component_separators = { left = '|', right = '|'},
-        section_separators = { left = '', right = '' },
         ignore_focus = {
             'NvimTree'
+        },
+        globalstatus = true
+    },
+    tabline = {
+        lualine_a = {
+            {
+                'tabs',
+                max_length = vim.o.columns,
+                mode = 2,
+                fmt = function(name, context)
+                    -- Show + if buffer is modified in tab
+                    local buflist = vim.fn.tabpagebuflist(context.tabnr)
+                    local winnr = vim.fn.tabpagewinnr(context.tabnr)
+                    local bufnr = buflist[winnr]
+                    local mod = vim.fn.getbufvar(bufnr, '&mod')
+
+                    return name .. (mod == 1 and ' +' or '')
+                end
+            }
         }
     }
 }
