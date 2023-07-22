7ce53c35736ca2b68c9c2a0fdb6b93f2ee410c78
Edward Ji <jiziao6@gmail.com>
2023-07-22 21:59:43 +0800

    Customize lualine some more

--- .config/nvim/after/plugin/lualine.lua
+++ .config/nvim/after/plugin/lualine.lua
@@ -1,10 +1,32 @@
+telescope_builtin = require('telescope.builtin')
+
 require('lualine').setup {
     options = {
         theme = 'auto',
-        ignore_focus = {
-            'NvimTree'
+        ignore_focus = {"NvimTree"},
+        globalstatus = true,
+        refresh = {
+            statusline = 250,
+            tabline = 250,
+            winbar = 250,
+        }
+    },
+    sections = {
+        lualine_a = {'mode'},
+        lualine_b = {
+            {
+                'branch',
+                on_click = function(_, _, _)
+                    telescope_builtin.git_branches()
+                end
+            },
+            'diff',
+            'diagnostics'
         },
-        globalstatus = true
+        lualine_c = {'filename'},
+        lualine_x = {'encoding', 'fileformat', 'filetype'},
+        lualine_y = {'progress'},
+        lualine_z = {'location'}
     },
     tabline = {
         lualine_a = {
@@ -12,6 +34,12 @@ require('lualine').setup {
                 'buffers',
                 mode = 2,
                 max_length = vim.o.columns * 2 / 3,
+                filetype_names = {
+                    NvimTree = 'NvimTree',
+                    TelescopePrompt = 'Telescope',
+                    fugitive = 'Fugitive',
+                    packer = 'Packer'
+                },
                 symbols = {
                     modified = ' +'
                 }
@@ -30,9 +58,7 @@ require('lualine').setup {
     },
     winbar = {
         lualine_a = {},
-        lualine_b = {
-            'searchcount'
-        },
+        lualine_b = {'searchcount'},
         lualine_c = {},
         lualine_x = {},
         lualine_y = {},
