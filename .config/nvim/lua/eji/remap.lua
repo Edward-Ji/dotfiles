8a002696a50f8c3102669bed85d9348bfa213222
Edward Ji <jiziao6@gmail.com>
2023-07-24 08:26:31 +0800

    Remove redundant silent option in buffer keymaps

--- .config/nvim/lua/eji/remap.lua
+++ .config/nvim/lua/eji/remap.lua
@@ -5,11 +5,11 @@ vim.keymap.set('v', '<M-j>', ":move '>+1<CR>gv=gv", {silent = true})
 vim.keymap.set('v', '<M-k>', ":move '<-2<CR>gv=gv", {silent = true})
 
 -- Buffer management
-vim.keymap.set('n', '<Tab>', "<Cmd>bnext<CR>", {silent = true})
-vim.keymap.set('n', '<S-Tab>', "<Cmd>bprevious<CR>", {silent = true})
+vim.keymap.set('n', '<Tab>', "<Cmd>bnext<CR>")
+vim.keymap.set('n', '<S-Tab>', "<Cmd>bprevious<CR>")
 for i = 1, 10 do
-    local lhs = "<Leader>" .. (i == 10 and 0 or i)
-    local rhs = "<Cmd>LualineBuffersJump " .. i .. "<CR>"
-    vim.keymap.set('n', lhs, rhs, {silent = true})
+    local lhs = "<Leader>b" .. (i == 10 and 0 or i)
+    local rhs = "<Cmd>silent! LualineBuffersJump " .. i .. "<CR>"
+    vim.keymap.set('n', lhs, rhs)
 end
-vim.keymap.set('n', '<Leader>d', "<Cmd>bdelete<CR>", {silent = true})
+vim.keymap.set('n', '<Leader>bd', "<Cmd>bdelete<CR>")
