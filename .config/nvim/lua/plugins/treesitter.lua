9e2e29bfa70d927972545751698192c8f4b795e2
Edward Ji <jiziao6@gmail.com>
2024-02-28 23:07:38 +1100

    Remove neovim tree sitter ensure installed

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -3,9 +3,6 @@ return {
     build = ':TSUpdate',
     config = function() 
         require("nvim-treesitter.configs").setup({
-            -- A list of parser names, or "all" (the five listed parsers should always be installed)
-            ensure_installed = 'all',
-
             -- Install parsers synchronously (only applied to `ensure_installed`)
             sync_install = false,
 
