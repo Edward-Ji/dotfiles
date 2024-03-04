94576dd142415d18545c651ca7cd151f0ce33aed
Edward Ji <jiziao6@gmail.com>
2024-03-04 22:40:43 +1100

    Add r.nvim plugin

---
+++ .config/nvim/lua/plugins/r.lua
@@ -0,0 +1,19 @@
+return {
+    'R-nvim/R.nvim',
+    config = function()
+        local opts = {
+            R_args = {"--quiet"},
+            hook = {
+                after_config = function ()
+                    if vim.o.syntax ~= "rbrowser" then
+                        vim.api.nvim_buf_set_keymap(0, "n", "<Enter>", "<Plug>RDSendLine", {})
+                        vim.api.nvim_buf_set_keymap(0, "v", "<Enter>", "<Plug>RSendSelection", {})
+                    end
+                end
+            },
+            rconsole_width = math.floor(vim.fn.winwidth(0) / 2)
+        }
+        require("r").setup(opts)
+    end,
+    lazy = false
+}
