52ad1e1e6ceda377fdb3e18fc559866376ca64d7
Edward Ji <jiziao6@gmail.com>
2024-03-10 12:45:11 +1100

    Add true color option in ipython config

--- .ipython/profile_default/ipython_config.py
+++ .ipython/profile_default/ipython_config.py
@@ -6,3 +6,6 @@ c.TerminalInteractiveShell.term_title = False
 c.TerminalInteractiveShell.editing_mode = 'vi'
 c.TerminalInteractiveShell.emacs_bindings_in_vi_insert_mode = False
 c.TerminalInteractiveShell.timeoutlen = 0.25
+
+## Use 24bit colors instead of 256 colors in prompt highlighting.
+c.TerminalInteractiveShell.true_color = True
