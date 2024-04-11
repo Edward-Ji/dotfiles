2e72b47a635a13784c9fde2c2a2e1c98ecc73847
Edward Ji <jiziao6@gmail.com>
2024-04-11 10:47:33 +1000

    Add clear terminal zle widget and binding

--- .zsh/bindings.zsh
+++ .zsh/bindings.zsh
@@ -1,3 +1,7 @@
 bindkey -v
 bindkey '^R' history-incremental-search-backward 
 bindkey '^S' history-incremental-search-forward
+
+clear-terminal() { tput reset; zle redisplay; }
+zle -N clear-terminal
+bindkey '^L' clear-terminal
