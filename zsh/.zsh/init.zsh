e70fec04fe568d4491ab26fc401c4d9635727d0c
Edward Ji <jiziao6@gmail.com>
2024-04-11 16:27:31 +1000

    Fix zsh init conditions
    Check if *env command exists after adding their bin to path.

--- zsh/.zsh/init.zsh
+++ zsh/.zsh/init.zsh
@@ -4,15 +4,15 @@
 # inserted to verify if respective commands are installed.
 
 # pyenv is a version manager for python
+export PYENV_ROOT="$HOME/.pyenv"
+[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
 if type pyenv &>/dev/null; then
-    export PYENV_ROOT="$HOME/.pyenv"
-    export PATH="$PYENV_ROOT/bin:$PATH"
     eval "$(pyenv init -)"
 fi
 
 # jenv is a version manager for java
+[[ -s "$HOME/.jenv/bin/jenv" ]] && export PATH="$HOME/.jenv/bin:$PATH"
 if type jenv &>/dev/null; then
-    export PATH="$HOME/.jenv/bin:$PATH"
     eval "$(jenv init -)"
 fi
 
