b9a0a75f70bb65c4f3c5621d062db0b7491d9a03
Edward Ji <jiziao6@gmail.com>
2024-04-25 21:35:31 +1000

    Remove nvim copilot default configurations

--- nvim/.config/nvim/lua/plugins/copilot.lua
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -5,46 +5,17 @@ return {
     config = function()
         require('copilot').setup({
             panel = {
-                enabled = true,
-                auto_refresh = false,
-                keymap = {
-                    jump_prev = '[[',
-                    jump_next = ']]',
-                    accept = '<CR>',
-                    refresh = 'gr',
-                    open = '<M-CR>',
-                },
                 layout = {
                     position = 'right',
-                    ratio = 0.5,
                 },
             },
             suggestion = {
-                enabled = true,
                 auto_trigger = true,
-                debounce = 75,
                 keymap = {
-                    accept = '<M-l>',
-                    accept_word = false,
-                    accept_line = false,
-                    next = '<M-]>',
-                    prev = '<M-[>',
-                    dismiss = '<C-]>',
+                    accept_word = '<M-Right>',
+                    accept_line = '<C-M-Right>',
                 },
             },
-            filetypes = {
-                yaml = false,
-                markdown = false,
-                help = false,
-                gitcommit = false,
-                gitrebase = false,
-                hgcommit = false,
-                svn = false,
-                cvs = false,
-                ['.'] = false,
-            },
-            copilot_node_command = 'node',
-            server_opts_overrides = {},
         })
     end,
     enabled = os.getenv('NVIM_MINIMAL') == nil,
