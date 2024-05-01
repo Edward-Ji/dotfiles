92432a93d0c602ed58454bb0220cc2918bce3656
Edward Ji <jiziao6@gmail.com>
2024-05-01 22:19:13 +1000

    Change lua formatter to stylua and add config

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -2,7 +2,7 @@ return {
     'stevearc/conform.nvim',
     opts = {
         formatters_by_ft = {
-            lua = { 'luafmt' },
+            lua = { 'stylua' },
             python = { 'isort', 'black' },
             rust = { 'rustfmt' },
         },
