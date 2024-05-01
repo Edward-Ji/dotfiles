f39cdd07e5f93675b18a30ed3d5f0fc4118704f6
Edward Ji <jiziao6@gmail.com>
2024-05-01 22:50:46 +1000

    Fix conform format mapping without lsp attach
    Before this commit, the conform format mapping is set in the LSP attach
    event. However, it's possible only a formatter is installed for the
    language.

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -1,5 +1,17 @@
 return {
     'stevearc/conform.nvim',
+    event = { 'BufWritePre' },
+    cmd = { 'ConformInfo' },
+    keys = {
+        {
+            '<F3>',
+            function()
+                require('conform').format({ async = true, lsp_fallback = true })
+            end,
+            mode = '',
+            desc = 'Format buffer',
+        },
+    },
     opts = {
         formatters_by_ft = {
             lua = { 'stylua' },
