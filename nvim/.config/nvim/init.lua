890155b67dc65791d199637364a07edb00a0b2d7
Edward Ji <jiziao6@gmail.com>
2024-06-28 22:52:45 +1000

    Add nvim lazy hererocks option

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -11,6 +11,9 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require('lazy').setup('plugins')
+require('lazy').setup({
+    spec = { { import = 'plugins' } },
+    rocks = { hererocks = true },
+})
 
 require('eji')
