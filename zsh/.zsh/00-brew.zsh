b81e26617e064b2a91920bdadf26db9f391e5712
Edward Ji <jiziao6@gmail.com>
2024-07-27 10:51:12 +1000

    Fix brew init on i386

--- zsh/.zsh/00-brew.zsh
+++ zsh/.zsh/00-brew.zsh
@@ -1,7 +1,7 @@
 # homebrew is a package manager for macOS
 if [ "$(arch)" = "arm64" ]; then
     BREW_PATH="/opt/homebrew/bin/brew"
-elif [ "$(arch)" = "x86_64" ]; then
+elif [ "$(arch)" = "i386" ]; then
     BREW_PATH="/usr/local/bin/brew"
 fi
 if [[ -f $BREW_PATH && -x $BREW_PATH ]]; then
